1872881ef2217224be9aae772250d295cd434764
Omar Alsyouf <omaralsoyouf@gmail.com>
2022-02-17 15:14:16 +0000

    final submission

--- index.js
+++ index.js
@@ -3,11 +3,8 @@ const form = document.querySelector('.form')
 
 function init () {
     fetch("http://localhost:3000/todos")
-        .then(function (res) {
-            console.log(res)
-            return res.json()
-        })
-        .then(x => addTodos(x));
+        .then(res => res.json())
+        .then(x => addTodos(x))
 }
 
 init()
@@ -17,15 +14,67 @@ function addTodos (todos) {
 }
 
 function addTodo (todo) {
+    const container = document.createElement('div')
     const li = document.createElement('li')
-    li.innerText = todo.title
+    const span = document.createElement('span')
+    const cmpBtn = document.createElement('button')
+    const delBtn = document.createElement('button')
+    cmpBtn.classList.add('btn')
+    delBtn.classList.add('btn')
+    delBtn.innerText = 'Delete'
+    delBtn.addEventListener('click', () => {
+        fetch("http://localhost:3000/todos/" + todo.id, {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }).then(() => li.remove())
+    })
+    cmpBtn.addEventListener('click', () => {
+        const updatedTodo = { completed: !todo.completed }
+        const options = {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(updatedTodo)
+        }
+        fetch("http://localhost:3000/todos/" + todo.id, options)
+            .then(() => {
+                todo.completed = updatedTodo.completed
+                if (todo.completed) {
+                    li.classList.add('completed')
+                    cmpBtn.innerText = 'uncomplete'
+                }
+                else {
+                    li.classList.remove('completed')
+                    cmpBtn.innerText = 'completed'
+                }
+
+            })
+    })
+    if (todo.completed) {
+        li.classList.add('completed')
+        cmpBtn.innerText = 'uncomplete'
+    }
+    else {
+        li.classList.remove('completed')
+        cmpBtn.innerText = 'completed'
+    }
+
+
+
+    container.classList.add('todo-container')
+    span.innerText = todo.title
+
+    container.append(cmpBtn, span, delBtn)
+    li.append(container)
     ul.append(li)
 }
 
 
 form.addEventListener('submit', (e) => {
     e.preventDefault()
-    console.log(form.title.value)
     const todo = {
         title: form.title.value,
         "completed": false
